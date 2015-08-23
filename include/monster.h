a8f6718c7486ceeb5f5317f6ffe0e5a080992a7e
Accácio <raccacio2@gmail.com>
2015-08-23 14:50:26 -0300

    monster class created

---
+++ include/monster.h
@@ -0,0 +1,14 @@
+#ifndef MONSTER_H
+#define MONSTER_H
+
+
+class Monster
+{
+    public:
+        Monster();
+        virtual ~Monster();
+    protected:
+    private:
+};
+
+#endif // MONSTER_H
---
+++ src/monster.cpp
@@ -0,0 +1,11 @@
+#include "monster.h"
+
+Monster::Monster()
+{
+    //ctor
+}
+
+Monster::~Monster()
+{
+    //dtor
+}
